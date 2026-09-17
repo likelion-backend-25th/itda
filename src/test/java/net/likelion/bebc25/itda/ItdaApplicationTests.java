b8db93730a8ddc0d7cde469d0ee104418ad15a26
Lee SeungEon <dltmddjs98@naver.com>
2026-09-17 13:34:37 +0900

    first commit

---
+++ src/test/java/net/likelion/bebc25/itda/ItdaApplicationTests.java
@@ -0,0 +1,13 @@
+package net.likelion.bebc25.itda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItdaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
