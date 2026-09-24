15927864d4a1f4f36756f12e66c9e2dacfcb4bae
LEEJIWON <caelumean215@gmail.com>
2026-09-24 17:58:09 +0900

    add infinity scroll

--- src/main/java/net/likelion/bebc25/itda/post/mapper/PostMapper.java
+++ src/main/java/net/likelion/bebc25/itda/post/mapper/PostMapper.java
@@ -2,6 +2,9 @@ package net.likelion.bebc25.itda.post.mapper;
 
 import net.likelion.bebc25.itda.post.domain.Post;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 @Mapper
 public interface PostMapper {
@@ -11,4 +14,15 @@ public interface PostMapper {
 
     // 2. 게시글 단건 조회 (ID)
     Post findById(long id);
+
+    // 3. 비로그인 시 보이는 메인 게시글(구독자 전용 게시글을 제외한 모든 게시글만 보임)
+    List<Post> findPublicPostsByCursor(@Param("cursor") Long cursor, @Param("size") int size);
+
+    // 3.1 로그인 시 보이는 메인 게시글(구독자 전용 게시글 + 일반 게시글, size = 게시글수)
+    List<Post> findSubscribedPostsByCursor(@Param("memberId")Long memberId,
+                                       @Param("cursor") Long cursor,
+                                       @Param("size")int size);
+
+
+
 }
