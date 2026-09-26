b92542f5027b23869243541a71219dc39b9370db
Lee SeungEon <dltmddjs98@naver.com>
2026-09-26 15:59:04 +0900

    update

--- src/main/java/net/likelion/bebc25/itda/theme/mapper/ThemeMapper.java
+++ src/main/java/net/likelion/bebc25/itda/theme/mapper/ThemeMapper.java
@@ -14,9 +14,10 @@ import java.util.List;
 public interface ThemeMapper {
 
     // 테마 전체 목록 조회
-    List<ThemeResponse> findAllThemes(@Param("memberId") Long memberId,
-                                              @Param("limit") int limit,
-                                              @Param("offset") int offset);
+    List<ThemeResponse> findAllThemes(@Param("memberId") Long memberId, @Param("limit") int limit, @Param("offset") int offset);
+
+    // ID 기반 테마 단건 조회
+    ThemeResponse findById(@Param("id") Long id);
 
     long countThemes();
 
